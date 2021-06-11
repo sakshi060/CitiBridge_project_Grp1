e3f51235daad60c8174768512f8d2f5779d030e4
Sakshi <sakshi.6.ingole@gmail.com>
2021-06-11 23:43:25 +0530

    Final Updated code

--- src/main/java/com/citi/demo/controller/StockDetailsController.java
+++ src/main/java/com/citi/demo/controller/StockDetailsController.java
@@ -98,7 +98,5 @@ public class StockDetailsController {
 			e.printStackTrace();
 			return null;
 		}
-
 	}
-
 }
--- src/main/java/com/citi/demo/controller/UserHistoryController.java
+++ src/main/java/com/citi/demo/controller/UserHistoryController.java
@@ -146,7 +146,4 @@ public class UserHistoryController {
 			return false;
 		}
 	}
-
-
-
 }

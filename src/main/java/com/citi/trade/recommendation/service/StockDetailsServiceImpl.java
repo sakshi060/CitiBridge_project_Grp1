48e15792cd6a6acdf771e9d8bee726b35ed8582e
Sakshi <sakshi.6.ingole@gmail.com>
2021-06-22 18:36:25 +0530

    Modified null check

--- src/main/java/com/citi/trade/recommendation/service/StockDetailsServiceImpl.java
+++ src/main/java/com/citi/trade/recommendation/service/StockDetailsServiceImpl.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
